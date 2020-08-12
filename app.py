b8716e8baed1ef61eb8443f2ecffbe8ac2609bfc
ZainShabbir <zainansari08@gmail.com>
2020-08-12 14:36:46 +0500

    Add Database Models And SQLAlchemy

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, url_for, session
-from flask_sqlalchemy import SQLAlchemy
+from models import session, User, Note
 import os
 import hashlib
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////test.db"
-db = SQLAlchemy(app)
 
 
 @app.route("/")
--- models.py
+++ models.py
@@ -1,8 +1,33 @@
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(200), unique=True, nullable=False)
-    email = db.Column(db.String(200), unique=True, nullable=False)
-    hash = db.Column(db.String(200), nullable=False)
+from sqlalchemy import create_engine, Column, Integer, ForeignKey, String
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-    def __str__(self):
-        return "<User %r>" % self.username
+engine = create_engine("sqlite:///test.db", echo=True)
+Base = declarative_base()
+Base.metadata.bind = engine
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+    hash = Column(String)
+
+    def __repr__(self):
+        return "<User(name='%s', email='%s')>" %s (self.name, self.email) 
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    content = Column(String)
+
+    user = relationship("User", back_populates="notes")
+
+    def __repr__(self):
+        return "<Note(user_id='%s', content='%s')>" %s (self.user_id, self.content)
