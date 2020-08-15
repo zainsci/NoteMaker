e4f83e0fbe91b3f04efb686cf9ac30539fc5718f
ZainShabbir <zainansari08@gmail.com>
2020-08-15 12:49:30 +0500

    Complete SignIn SignUp Routes

--- app.py
+++ app.py
@@ -1,39 +1,128 @@
-from flask import Flask, render_template, url_for, request, redirect, jsonify
-from models import session, User, Note
+from flask import Flask, render_template, url_for, request, redirect, jsonify, session, flash
+from models import Session, db, User, Note
 import os
 import hashlib
 
 app = Flask(__name__)
+app.secret_key = b'\xd9|\x1b8\x13r\x95\x97\xee\x17"`\x8cdHh'
+
+# Setting up session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 
 
+# Home Or Default Route
 @app.route("/")
 def index():
+    if session.get("username"):
+        return render_template("dashboard.html")
     return render_template("index.html")
 
 
-@app.route("/signin")
+# User Sign In Route
+@app.route("/signin", methods=["GET", "POST"])
 def signin():
+    # First clear session
+    session.clear()
+
+    # If User tries to signin
     if request.method == "POST":
-        return redirect("index")
+        username = request.form['username']
+        password = request.form['password']
+
+        # If Username is not provided
+        if not username:
+            flash("Must Type A Username")
+            return render_template("index.html")
+        # If Password is not provided
+        elif not password:
+            flash("Must Type A Password")
+            return render_template("index.html")
+
+        # Querying Database for User with Username
+        user = db.query(User).filter_by(username=username).first()
+        db.commit()
+
+        # If Databse returned User object
+        if user:
+            # Hashing Password for password checking
+            hash = hashlib.md5(password.encode('utf-8')).hexdigest()
+
+            # matching password from databse and the user entered
+            if hash == user.hash:
+                session['username'] = username
+                return redirect(url_for('index'))
+            # if incorrect password
+            else:
+                flash("Invalid Username or Password")
+                return redirect(url_for("index"))
+        # if incorrect username
+        else:
+            flash("Invalid Username or Password")
+            return redirect(url_for("index"))
     else:
-        return render_template("index.html")
+        return redirect(url_for("index"))
 
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
+    # clearing session
+    session.clear()
+
+    # if user tries to signup
     if request.method == "POST":
-        return redirect("index")
+        # getting username and other
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        conf_password = request.form['conf-password']
+
+        # if username is not provided
+        if not username:
+            flash("Must Provide A Username")
+            return redirect(url_for("index"))
+        # or email is not provided
+        elif not email:
+            flash("Must Provide An Email")
+            return redirect(url_for("index"))
+        # if both password are provided
+        elif not password or not conf_password:
+            flash("Must Provide A Password And Confirmation Password")
+            return redirect(url_for("index"))
+        else:
+            # if both passwords are not same
+            if password != conf_password:
+                flash("Password Does Not Match")
+                return redirect(url_for("index"))
+            else:
+                # entering user to database
+                user = User()
+                user.username = username
+                user.email = email
+                user.hash = hashlib.md5((password).encode('utf-8')).hexdigest()
+                db.add(user)
+                db.commit()
+                # creating user session and logging in
+                session['username'] = username
+                flash(f"Account Created With Username {username}")
+                return redirect(url_for("index"))
     else:
-        return render_template("index.html")
+        return redirect("index")
 
 
 @app.route("/fetchnotes")
 def fetch_notes():
     if request.method == "POST":
-        notes = session.query(Note).all()
+        notes = db.query(Note).all()
         return jsonify({"notes": notes})
     else:
         pass
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,24 +1,29 @@
 from sqlalchemy import create_engine, Column, Integer, ForeignKey, String
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
-engine = create_engine("sqlite:///test.db", echo=True)
+engine = create_engine("sqlite:///test.db", echo=True, pool_pre_ping=True)
 Base = declarative_base()
 Base.metadata.bind = engine
 
 Session = sessionmaker(bind=engine)
-session = Session()
+db = Session()
+
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    username = Column(String)
     email = Column(String)
     hash = Column(String)
+    notes = relationship("Note", back_populates='users')
 
     def __repr__(self):
-        return "<User(name='%s', email='%s')>" %s (self.name, self.email) 
+        return "<User(username='%s', email='%s')>" % (self.username,
+                                                      self.email)
+
 
 class Note(Base):
     __tablename__ = "notes"
@@ -26,8 +31,10 @@ class Note(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     content = Column(String)
+    timestamp = Column(TIMESTAMP)
 
-    user = relationship("User", back_populates="notes")
+    users = relationship("User", back_populates="notes")
 
     def __repr__(self):
-        return "<Note(user_id='%s', content='%s')>" %s (self.user_id, self.content)
+        return "<Note(user_id='%s', content='%s')>" % (self.user_id,
+                                                       self.content)
